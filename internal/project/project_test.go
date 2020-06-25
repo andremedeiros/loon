29156a8a074675ac9639a4019eb6da4b1ba72ae3
Andre Medeiros <me@andremedeiros.info>
2020-06-24 20:41:47 -0400

    Fix tests

--- internal/project/project_test.go
+++ internal/project/project_test.go
@@ -1,13 +1,22 @@
 package project
 
 import (
+	"io/ioutil"
+	"os"
 	"reflect"
 	"testing"
 )
 
+func fromPayload(payload string) (*Project, error) {
+	tmp, _ := ioutil.TempFile("", "loon.yml")
+	defer os.Remove(tmp.Name())
+	tmp.WriteString(payload)
+	return fromPath(tmp.Name())
+}
+
 func TestNewFromPayload(t *testing.T) {
 	t.Run("invalid payload", func(t *testing.T) {
-		if _, err := fromPayload([]byte(`!`)); err == nil {
+		if _, err := fromPayload(`!`); err == nil {
 			t.Errorf("expected error but didn't get one")
 		}
 	})
@@ -19,7 +28,7 @@ environment:
   SOME: value
   SOME_OTHER: other value
 `
-		p, err := fromPayload([]byte(payload))
+		p, err := fromPayload(payload)
 		if err != nil {
 			t.Errorf("got %q", err)
 			return
@@ -48,7 +57,7 @@ services:
     version: 6.0.4
 `
 
-		p, err := fromPayload([]byte(payload))
+		p, err := fromPayload(payload)
 		if err != nil {
 			t.Errorf("got %q", err)
 			return
@@ -67,7 +76,7 @@ tasks:
   compile:
     command: compile the thing
 `
-		p, err := fromPayload([]byte(payload))
+		p, err := fromPayload(payload)
 		if err != nil {
 			t.Errorf("got %q", err)
 			return
