5ead1a5356d3af26a0896416000bfc4a31eb21f1
Andre Medeiros <me@andremedeiros.info>
2020-07-06 14:01:32 -0400

    fix redis version check

--- spec/dependencies/redis_spec.rb
+++ spec/dependencies/redis_spec.rb
@@ -4,12 +4,12 @@ describe 'Dependencies' do
 
     versions.each do |version|
       it "installs version #{version} correctly" do
-        test_dep 'redis', version: version, match: version
+        test_dep 'redis', cmd: 'redis-cli --version', version: version, match: version
       end
     end
 
     it "installs version #{latest} as the default" do
-      test_dep 'redis', match: latest
+      test_dep 'redis', cmd: 'redis-cli --version', match: latest
     end
   end
 end
