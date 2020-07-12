28a422d802d05b3c668a0f91d6c586b349e3fe80
Andre Medeiros <me@andremedeiros.info>
2020-07-12 09:46:00 -0400

    don't test for doctor sudo

--- spec/commands/doctor_spec.rb
+++ spec/commands/doctor_spec.rb
@@ -1,14 +1,5 @@
 describe 'Commands' do
   describe 'doctor', command: true do
-    it 'should show error when sudo fails' do
-      with_command_mock('sudo', 'exit 1') do
-        loon %w(doctor)
-      end
-
-      assert_stderr 'sudo not enabled for this user'
-      assert_status 0
-    end
-
     it 'should show error when nix is not installed' do
       with_environment(path: '') do
         loon %w(doctor)
