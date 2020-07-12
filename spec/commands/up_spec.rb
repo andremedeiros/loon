a38f74c05632d867c7e81f582b393fd9972a8013
Andre Medeiros <me@andremedeiros.info>
2020-07-12 08:27:48 -0400

    fix ifconfig on up test too

--- spec/commands/up_spec.rb
+++ spec/commands/up_spec.rb
@@ -12,8 +12,13 @@ describe 'Commands' do
       loon %(up)
       ip = project_ip
 
-      expect(`ifconfig`).to include(ip)
+      ifconfig = if OS.linux?
+        `ip addr list lo`
+      else
+        `ifconfig lo0`
+      end
+
+      expect(ifconfig).to include(ip)
     end
   end
 end
-
