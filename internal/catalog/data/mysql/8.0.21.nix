e1fa2f495fbf73e495af918cf83fe85afaffd504
Andre Medeiros <me@andremedeiros.info>
2020-07-15 19:46:07 -0400

    Fix mysql

--- internal/catalog/data/mysql/8.0.21.nix
+++ internal/catalog/data/mysql/8.0.21.nix
@@ -5,6 +5,8 @@ loon-mysql-8_0_21 = pkgs.mysql80.overrideAttrs(attrs: {
     url = "https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-8.0.21.tar.gz";
     sha256 = "0d00k55rkzdgn5wj32vxankjk5x3ywfqw62zxzg3m503xrg56mmd";
   };
+  patches = [];
+  buildInputs = [pkgs.boost172] ++ attrs.buildInputs;
 });
 #{{ end }}
 
